881584ccc72956e5e8521b564af61698235ec3a4
catawaldman <catalinawaldman@hotmail.com>
2026-03-03 11:55:49 -0300

    Ruta de la app: registrarse -> chat -> cerrar sesion -> login

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,19 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from "../views/Home"
-import { NotFound } from "../views/NotFound"
-import { Login } from "../views/Login"
-import { RouteProtected } from "../components/RouteProtected"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "../views/Home";       
+import { NotFound } from "../views/NotFound"; 
+import { Login } from "../views/Login";       
+import Registro from "../pages/Registro";    
+import { RouteProtected } from "../components/RouteProtected";
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
 
 const RouterApp = () => {
+  const { loggedUser } = useContext(ChatContext);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RouteProtected><Home /></RouteProtected>} />
+    
+        <Route
+          path="/"
+          element={loggedUser ? <Navigate to="/chat" replace /> : <Registro />}
+        />
+
+        {/* Chat protegido */}
+        <Route
+          path="/chat"
+          element={
+            <RouteProtected>
+              <Home />
+            </RouteProtected>
+          }
+        />
+
+        {/* Login */}
         <Route path="/login" element={<Login />} />
+
+        {/* Registro */}
+        <Route path="/registro" element={<Registro />} />
+
+        {/* Fallback */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export { RouterApp }
+export { RouterApp };
